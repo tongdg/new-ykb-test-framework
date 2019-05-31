842a2fee31b56171529c6e1f7952ccf89718d08b
Zhangfukai <616032226@qq.com>
2019-05-31 14:36:48 +0800

    修改命名名称

--- test_case/pc/zfk/zfk_login_case.py
+++ test_case/pc/zfk/zfk_login_case.py
@@ -4,20 +4,21 @@
 '2019-05-29 Created by Zhangfukai'
 
 import unittest
-from pages.pc_pages.login_HouTaipage import Login_HouTaiPage
+from pages.pc_pages.zfk_login_HouTaipage import Login_HouTaiPage
 from selenium import webdriver
 import os
-
+import time
 class LoginCase(unittest.TestCase):
     def setUp(self):
         self.longin_page = Login_HouTaiPage(webdriver.Chrome(),os.getcwd())
         self.longin_page.login_person()
-        print(os.getcwd())
+        time.sleep(500000)
+
 
     def test_login_case(self):
         pass
     def tearDown(self):
-        self.longin_page.quit()
+        print("登录管理后台登录成功")
 if __name__=="__main__":
     unittest.main()
 
